cff210152c902dfb077b96bbd5da80d9b6d494cb
khaibtros <trankhai15052005@gmail.com>
2025-05-17 15:11:39 +0700

    Database connect

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,10 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Database connected!"));
+
 const app = express();
 
 app.use(cors());
@@ -10,5 +14,5 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(7000, () => {
-  console.log("server started on localhost:7000");
+  console.log("Server running on localhost:7000");
 });
