33aa7514c79415a6a4202ac02a09e56d35924779
khaibtros <trankhai15052005@gmail.com>
2025-05-19 14:28:53 +0700

    Mobile navbar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -10,7 +11,9 @@ const Header = () => {
         >
           QuickMunch
         </Link>
-
+        <div className="md:hidden">
+          <MobileNav />
+        </div>
       </div>
     </div>
   );
