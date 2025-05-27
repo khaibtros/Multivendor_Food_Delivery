d711d160dca3ef354bbfef1987b4c708cc4ca8b7
thanhdthe173427 <thanhdthe173427@fpt.edu.vn>
2025-05-27 15:45:19 +0700

    user

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 const AppRoutes = () => {
   return (
     <Routes>
@@ -16,7 +17,8 @@ const AppRoutes = () => {
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
 
-      <Route
+        <Route element={<ProtectedRoute/>} >
+           <Route
         path="/user-profile"
         element={
           <Layout>
@@ -24,6 +26,8 @@ const AppRoutes = () => {
           </Layout>
         }
       />
+        </Route>
+     
 
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
