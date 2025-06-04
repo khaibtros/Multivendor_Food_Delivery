eb6008879624d3f32a233faf682799e42112cf91
thanhdthe173427 <thanhdthe173427@fpt.edu.vn>
2025-06-04 21:07:02 +0700

    Pagination

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,19 +1,42 @@
-import Info from "../components/HomeScreen/Info.tsx";
-import CardComponent from "../components/HomeScreen/CardComponent.tsx";
-import Faq from "../components/HomeScreen/FAQ.tsx";
+import landingImage from "../assets/landing.png";
+import appDownloadImage from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
   return (
     <div className="flex flex-col gap-12">
       <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
-        <h1 className="text-5xl font-bold tracking-tight text-red-600">
-
+        <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+          Tuck into a takeway today
         </h1>
-        <span className="text-xl"></span>
+        <span className="text-xl">Food is just a click away!</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+        />
       </div>
-      <Info />
-      <CardComponent />
-      <Faq />
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={landingImage} />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tighter">
+            Order takeaway even faster!
+          </span>
+          <span>
+            Download the MernEats App for faster ordering and personalised
+            recommendations
+          </span>
+          <img src={appDownloadImage} />
+        </div>
+      </div>
     </div>
   );
 };
