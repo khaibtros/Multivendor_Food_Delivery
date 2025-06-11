f35f78df733abdd2530dcc1a31a1a4e3fcc3d49a
theanhtruong04 <theanhtruong16112004@gmail.com>
2025-06-11 14:51:27 +0700

    update restaurant

---
+++ Multivendor_Food_Delivery-main/frontend/src/components/OrderStatusHeader.tsx
@@ -0,0 +1,46 @@
+import { ORDER_STATUS } from "@/config/order-status-config";
+import { Order } from "@/types";
+
+type Props = {
+    order: Order;
+};
+
+const OrderStatusHeader = ({ order }: Props) => {
+   const getExpectedDelivery = () => {
+    let created = new Date (order.createdAt);
+
+    created.setMinutes(
+        created.getMinutes() + order.restaurant.estimatedDeliveryTime
+    );
+
+    const hours = created.getHours();
+    const minutes = created.getMinutes();
+    
+    // hours = 12
+    // minutes =1
+    // 12: 2
+    // 12: 02
+    const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    return `${hours}:${paddedMinutes}`; //12 : 05
+   };
+
+   const getOrderStatusInfo = () => {
+    return(
+    ORDER_STATUS.find((o) => o.value === order.status) || ORDER_STATUS[0]
+    );
+   };
+
+    return (
+        <>
+            <h1 className="text-4xl font-bold tracking-tighter flex flex-col gap-5 md:flex-row md:justify-between">
+                <span> Order Status: {getOrderStatusInfo().label} </span>
+                <span> Expexted by: {getExpectedDelivery()} </span>
+            </h1>
+            <Progress 
+            className="animate-pulse" 
+            value={getOrderStatusInfo().progressValue} 
+            />
+        </>
+    );
+};
